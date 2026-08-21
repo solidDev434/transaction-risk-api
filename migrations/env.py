6546fc4c7105738247c51424682b6df175404dbd
Joseph Ibok <josephibok75@gmail.com>
2026-08-21 14:29:01 +0100

    Fixed and migrated wallet model

--- migrations/env.py
+++ migrations/env.py
@@ -9,6 +9,7 @@ from sqlmodel import SQLModel
 from alembic import context
 
 from app.users.model import User
+from app.wallet.model import Wallet
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
